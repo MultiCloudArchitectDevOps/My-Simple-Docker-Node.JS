602ea06045bcc03724e187c9a42897323da44371
Yogeshwar Walmik Deshmukh <deshmukh.yogesh333@gmail.com>
2026-04-22 22:18:09 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js Docker App 🚀");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
